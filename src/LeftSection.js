b2f92e1332facd7eacc4b507802f1add12bc83dd
Fraylin <fraylinayala321@gmail.com>
2024-11-05 10:31:13 -0500

    Updates

--- src/LeftSection.js
+++ src/LeftSection.js
@@ -4,9 +4,20 @@ import plusIcon from "./icons/plus.png"
 import penIcon from "./icons/pen.png"
 /*Add the icons in the edit div*/
 function LeftSection() {
-    const [exercise, setExercise] = useState("");
+    const [exercises, setExercise] = useState(["","","","",""]);
 
     const exerciseList = ['Bench Press', 'Dumbell Press', 'Cable Raises', 'Dumbell Lateral Raises'];
+    let index=-1;
+
+
+
+    const handleExerciseChange = (value, index) => {
+        const updatedExercises = [...exercises];
+        updatedExercises[index] = value;
+        setExercise(updatedExercises);
+    }
+
+     
 
 
 
@@ -21,13 +32,14 @@ function LeftSection() {
             </div>
 
             <form>
-                <label>Exercise 1:</label>
+            {exercises.map((exercise, index) => {
+                <label>{"Exercise " + index+1 + ":"} </label>
                 <div className="exerciseGroup">
                     <div className="autocomplete">
-                        <input type="text" placeholder="Type exercise" className="exercises" onChange={(event) => setExercise(event.target.value)} value={exercise}></input>
+                        <input type="text" placeholder="Type exercise" className="exercises" onChange={(event) => setExercise(handleExerciseChange(event.target.value, ++index))} value={exercises}></input>
                         <div className='dropDown'>
                             {exerciseList.filter((item) => {
-                                const exerciseSearch = exercise.toLowerCase();
+                                const exerciseSearch = exercises[index].toLowerCase();
                                 const currExercise = item.toLowerCase()
                                 return (exerciseSearch && currExercise.startsWith(exerciseSearch) && currExercise != exerciseSearch)
 
@@ -38,8 +50,30 @@ function LeftSection() {
                   
                     <input type="text" placeholder="Sets" className="sets"></input>
                     <input type="text" placeholder="Reps" className="reps"></input>
-                </div>
+                </div> 
+                
 
+            })
+            /*
+                <label>Exercise 1:</label>
+                <div className="exerciseGroup">
+                    <div className="autocomplete">
+                        <input type="text" placeholder="Type exercise" className="exercises" onChange={(event) => setExercise(handleExerciseChange(event.target.value, ++index))} value={exercises}></input>
+                        <div className='dropDown'>
+                            exerciseList.filter((item) => {
+                                const exerciseSearch = exercises[index].toLowerCase();
+                                const currExercise = item.toLowerCase()
+                                return (exerciseSearch && currExercise.startsWith(exerciseSearch) && currExercise != exerciseSearch)
+
+                            }).map((mapExercise) => (
+                                <div className="dropdown-row" onClick={() => setExercise(mapExercise)}>{mapExercise}</div>))}
+                        </div>
+                    </div>
+                  
+                    <input type="text" placeholder="Sets" className="sets"></input>
+                    <input type="text" placeholder="Reps" className="reps"></input>
+                </div> 
+                }
 
 
                 <label>Exercise 2:</label>
@@ -47,13 +81,13 @@ function LeftSection() {
                     <div className="autocomplete">
                         <input type="text" placeholder="Type exercise" className="exercises" onChange={(event) => setExercise(event.target.value)} value={exercise}></input>
                         <div className='dropDown'>
-                            {exerciseList.filter((item) => {
+                            exerciseList.filter((item) => {
                                 const exerciseSearch = exercise.toLowerCase();
                                 const currExercise = item.toLowerCase()
                                 return (exerciseSearch && currExercise.startsWith(exerciseSearch) && currExercise != exerciseSearch)
 
                             }).map((mapExercise) => (
-                                <div className="dropdown-row" onClick={() => setExercise(mapExercise)}>{mapExercise}</div>))}
+                                <div className="dropdown-row" onClick={() =>// setExercise(mapExercise)}>{mapExercise}</div>))}
                         </div>
                     </div>
                   
@@ -64,15 +98,15 @@ function LeftSection() {
                 <label>Exercise 3:</label>
                 <div className="exerciseGroup">
                     <div className="autocomplete">
-                        <input type="text" placeholder="Type exercise" className="exercises" onChange={(event) => setExercise(event.target.value)} value={exercise}></input>
+                        <input type="text" placeholder="Type exercise" className="exercises" onChange=//{(event) => setExercise(event.target.value)} value={exercise}></input>
                         <div className='dropDown'>
-                            {exerciseList.filter((item) => {
+                            exerciseList.filter((item) => {
                                 const exerciseSearch = exercise.toLowerCase();
                                 const currExercise = item.toLowerCase()
                                 return (exerciseSearch && currExercise.startsWith(exerciseSearch) && currExercise != exerciseSearch)
 
                             }).map((mapExercise) => (
-                                <div className="dropdown-row" onClick={() => setExercise(mapExercise)}>{mapExercise}</div>))}
+                                <div className="dropdown-row" onClick={() => //setExercise(mapExercise)}>{mapExercise}</div>))}
                         </div>
                     </div>
                   
@@ -84,9 +118,9 @@ function LeftSection() {
                 <label>Exercise 4:</label>
                 <div className="exerciseGroup">
                     <div className="autocomplete">
-                        <input type="text" placeholder="Type exercise" className="exercises" onChange={(event) => setExercise(event.target.value)} value={exercise}></input>
+                        <input type="text" placeholder="Type exercise" className="exercises" onChange={(event) => //setExercise(event.target.value)} value={exercise}></input>
                         <div className='dropDown'>
-                            {exerciseList.filter((item) => {
+                            exerciseList.filter((item) => {
                                 const exerciseSearch = exercise.toLowerCase();
                                 const currExercise = item.toLowerCase()
                                 return (exerciseSearch && currExercise.startsWith(exerciseSearch) && currExercise != exerciseSearch)
@@ -103,21 +137,22 @@ function LeftSection() {
                 <label>Exercise 5:</label>
                 <div className="exerciseGroup">
                     <div className="autocomplete">
-                        <input type="text" placeholder="Type exercise" className="exercises" onChange={(event) => setExercise(event.target.value)} value={exercise}></input>
+                        <input type="text" placeholder="Type exercise" className="exercises" onChange=//{(event) => setExercise(event.target.value)} value={exercise}></input>
                         <div className='dropDown'>
-                            {exerciseList.filter((item) => {
+                            exerciseList.filter((item) => {
                                 const exerciseSearch = exercise.toLowerCase();
                                 const currExercise = item.toLowerCase()
                                 return (exerciseSearch && currExercise.startsWith(exerciseSearch) && currExercise != exerciseSearch)
 
                             }).map((mapExercise) => (
-                                <div className="dropdown-row" onClick={() => setExercise(mapExercise)}>{mapExercise}</div>))}
+                                <div className="dropdown-row" onClick={() =>// setExercise(mapExercise)}>{mapExercise}</div>))}
                         </div>
                     </div>
                   
                     <input type="text" placeholder="Sets" className="sets"></input>
                     <input type="text" placeholder="Reps" className="reps"></input>
                 </div>
+                */
 
                
 
