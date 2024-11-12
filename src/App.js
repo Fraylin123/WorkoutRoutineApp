a4f4780de05d0a80bd72fbc2718fcc75a734cc61
Fraylin <fraylinayala321@gmail.com>
2024-11-12 10:32:16 -0500

    Fixed buttons

--- src/App.js
+++ src/App.js
@@ -7,30 +7,39 @@ import { useState } from "react";
 
 
 function App() {
-    const [routineValues, setRoutineValues] = useState([false,false,false])
-    const [fitnessValues, setFitnessValues] = useState([false,false,false])
+    const [routineValues, setRoutineValues] = useState([false,false,false]);
+    const [fitnessValues, setFitnessValues] = useState([false,false,false]);
 
-    const handleClick = (event) => {
-        setRoutineValues([])
-        alert("Button clicked");
-        alert("The value is: " + event.target.value);
-
+    const handleRoutineClick = (index) => {
+        setRoutineValues((prev) => {
+            return prev.map((_,i) => i === index);
+        });
     }
+
+    const handleFitnessClick = (index) => {
+        setFitnessValues((prev) => {
+            return prev.map((_,i) => i === index);
+        });
+    }
+
+
     return (
         <>
             <Navigation />
             <h1 style={{ textAlign: 'center' }}>Workout Routine App</h1>
             <div className='routineType'>
-                <button onClick={handleClick} >PPL</button>
-                <button onClick={handleClick}>Upper Lower</button>
-                <button onClick={handleClick}>Bro Split</button>
+                <button onClick={() => handleRoutineClick(0)} style={{backgroundColor: routineValues[0] ? 'gray': 'black'}}>PPL</button>
+                <button onClick={() => handleRoutineClick(1)} style={{backgroundColor: routineValues[1] ? 'gray': 'black'}}>Upper Lower</button>
+                <button onClick={() => handleRoutineClick(2)} style={{backgroundColor: routineValues[2] ? 'gray': 'black'}}>Bro Split</button>
             </div>
-
+        {routineValues.some(value => value === true) && (
             <div className="fitnessLevel">
-                <button>Beginner</button>
-                <button>Intermediate</button>
-                <button>Advanced</button>
+                <button onClick={() => handleFitnessClick(0)} style={{backgroundColor: fitnessValues[0] ? 'gray': 'black'}}>Beginner</button>
+                <button onClick={() => handleFitnessClick(1)} style={{backgroundColor: fitnessValues[1] ? 'gray': 'black'}}>Intermediate</button>
+                <button onClick={() => handleFitnessClick(2)} style={{backgroundColor: fitnessValues[2] ? 'gray': 'black'}}>Advanced</button>
             </div>
+        )
+        }
 
             <div className='mainContainer'>
                 <h3>Monday</h3>
