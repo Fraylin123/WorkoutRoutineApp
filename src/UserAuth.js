9610dd966954c26cd3417f89804173d050793797
fraylin <fraylinayala321@gmail.com>
2025-02-26 22:19:44 -0500

    Finished form design, starting functionality

--- src/UserAuth.js
+++ src/UserAuth.js
@@ -1,29 +1,42 @@
 import { useState, useEffect } from "react";
 import "./UserAuth.css"
 function UserAuth(){
+    const [status, setStatus] = useState("login")
+
+
+
     return (
         <div className="authMain">
             <form className="user-authentication-container">
                 <div className="user-heading">
-                    <span>Login</span>
+                    <span>Sign in</span>
                     <div className="underline"></div>
                     </div>
                 <div className="inputs">
                     <div className="input">
-                        <input type="text" placeholder="Name"/>
+                        <input type="text" placeholder="Username"/>
                     </div>
-                    <div className="input">
-                        <input type="email" placeholder="Email"/>
-                    </div>
+                   
                     <div className="input">
                         <input type="password" placeholder="Password"/>
                     </div>
-                    
 
+                    <div className="input">
+                        <input type="password" placeholder="Confirm password"/>
+                    </div>
+
+                    <div className="input">
+                        <input type="email" placeholder="Email"/>
+                    </div>
+                    
                 </div>
                 <div className = "submit-container">
                     <button>Login</button>
+                    
                 </div>
+                <div className="sign-up">
+                        <button>Sign up</button>
+                    </div>
                 
             </form>
         </div>
