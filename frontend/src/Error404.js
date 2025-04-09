15c86aadf9c786919cf1872e96cd9fca1c8e2b2b
fraylin <fraylinayala321@gmail.com>
2025-04-09 11:41:30 -0400

    Adding Miguel the Marmot in Error page

--- frontend/src/Error404.js
+++ frontend/src/Error404.js
@@ -1,8 +1,12 @@
+import Miguel from "./marmot.jpg"
+
 function Error404() {
     return (
-        <div style={{ padding: "2rem", textAlign: "center", minHeight: "100vh"}}>
+        <div style={{ padding: "2rem", textAlign: "center"}}>
             <h1>404 - Page Not Found</h1>
-            <p>Oops! That page doesn’t exist.</p>
+            <img src={Miguel} alt="Miguel the Marmot" style={{maxWidth: "60%", maxHeight: "60%"}}/>
+            <h2>Miguel the Marmot</h2>
+            <span>This page doesn't exist buddy. By the way...you got crackers?</span>
         </div>
     );
 }
