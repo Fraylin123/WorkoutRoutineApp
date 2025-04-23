0871bfe5e5333099cadd01e5681523e076941ca7
fraylin <fraylinayala321@gmail.com>
2025-04-23 14:57:39 -0400

    Adding MySQL reconnection logic

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,21 +3,42 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 //MySQL Connection
-const accountsDb = mysql.createConnection({
-    host: process.env.cHost,
-    user: process.env.cUser,
-    password: process.env.cPassword,
-    database: process.env.cDatabase,
-    port: process.env.cPort,
-});
 
-accountsDb.connect((error) => {
-    if (error) {
-        console.error('Connection error:', error);
-    } else {
-        console.log('Succesful SQL connection');
-    }
-});
+let accountsDb;
+
+function handleConnection() {
+    accountsDb = mysql.createConnection({
+        host: process.env.cHost,
+        user: process.env.cUser,
+        password: process.env.cPassword,
+        database: process.env.cDatabase,
+        port: process.env.cPort,
+    });
+
+    accountsDb.connect((error) => {
+        if (error) {
+            console.error('Connection error:', error);
+            setTimeout(handleConnection, 2000)
+        } else {
+            console.log('Succesful SQL connection');
+        }
+    });
+    
+    accountsDb.on('error', err => {
+        console.log('MySQL error', err)
+        if (err.code = 'PROTOCOL_CONNECTION_LOST'){
+            handleConnection();
+        }
+        else{
+            throw err;
+        }
+        
+    });
+
+}
+
+handleConnection();
+
 
 //MongoDB Atlas connection
 mongoose
