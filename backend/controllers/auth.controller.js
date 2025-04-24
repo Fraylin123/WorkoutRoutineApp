41afae634472189310c9c9940335de61baf2eeb6
fraylin <fraylinayala321@gmail.com>
2025-04-24 12:35:52 -0400

    Preparing for aws deployment and adding build folder

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -26,8 +26,8 @@ const login = async (req, res) => {
             //Assigning cookie to authenticated user
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: true,
-                sameSite: 'none',
+                secure: false,
+                sameSite: 'lax',
             });
 
             res.json({
@@ -67,8 +67,8 @@ const register = async (req, res) => {
 const logout = async (req, res) => {
     res.clearCookie('token', {
         httpOnly: true,
-        secure: true,
-        sameSite: 'none',
+        secure: false,
+        sameSite: 'lax',
     });
     res.json({ message: 'Logged out successfully' });
 };
