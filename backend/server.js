d3cec66f9d32b1e5ff0fc9f2b1b4467c0261dae2
fraylin <fraylinayala321@gmail.com>
2025-04-24 12:18:34 -0400

    Changes to serve frontend through the backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const { accountsDb, mongoose } = require('./config/db'); //Automatically start the MySQL and MongoDB Atlas by importing them
 
 const app = express();
@@ -13,10 +14,15 @@ const port = process.env.PORT || 5000;
 app.use(cors({ origin: 'http://workout-routine-app-frontend.s3-website-us-east-1.amazonaws.com', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.set('trust proxy', 1);
-
 
 app.use('/api/exercises', require('./routes/exercises.routes'));
 app.use('/api/auth', require('./routes/auth.routes'));
+
+//Serve frontend through the backend
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
